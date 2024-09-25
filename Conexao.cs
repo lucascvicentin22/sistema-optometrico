529f5dcb5d7802e1d4e5bf727a43f8ce4265da57
LUCAS CHAGAS VICENTIN <129609514+lucascvicentin22@users.noreply.github.com>
2024-09-25 12:57:14 -0300

    Add files via upload

---
+++ Conexao.cs
@@ -0,0 +1,78 @@
+﻿using MySqlConnector;
+using System;
+using System.Data;
+using System.Windows;
+
+public class Conexao
+{
+    private MySqlConnection conn;
+    private string connectionString;
+    public Conexao()
+    {
+        connectionString = "Server = localhost; Database = db_clinica; User ID = root; Password = 2707; ";
+        conn = new MySqlConnection(connectionString);
+    }
+
+    public void OpenConnection()
+    {
+        if (conn.State == ConnectionState.Closed)
+        {
+            conn.Open();
+        }
+    }
+
+    public DataTable BuscarDados(string query)
+    {
+        DataTable dt = new DataTable();
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+        }
+
+        return dt;
+    }
+
+    public void CloseConnection()
+    {
+        if (conn.State == ConnectionState.Open)
+        {
+            conn.Close();
+        }
+    }
+
+    public void ExecuteQuery(string query, params MySqlParameter[] parametros)
+    {
+        try
+        {
+            OpenConnection();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                cmd.Parameters.AddRange(parametros);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Erro ao executar a query: " + ex.Message);
+        }
+        finally
+        {
+            CloseConnection();
+        }
+    }
+}
