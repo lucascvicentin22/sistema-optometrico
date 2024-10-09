8b13d8401132d4e5172d5b8206f4afd1f973f7c9
LUCAS CHAGAS VICENTIN <129609514+lucascvicentin22@users.noreply.github.com>
2024-10-09 17:47:35 -0300

    Update frmPreCadastro.cs

--- frmPreCadastro.cs
+++ frmPreCadastro.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using MySqlConnector;
 
 namespace SistemaOptometrico
 {
     public partial class frmPreCadastro : Form
     {
-        private string connectionString = "Server=localhost;Database=db_clinica;User ID=root;Password=2707;";
+        private Conexao conexao = new Conexao();
+
         public frmPreCadastro()
         {
             InitializeComponent();
@@ -21,6 +16,7 @@ namespace SistemaOptometrico
             txtNASCIMENTO.Mask = "00/00/0000";
             txtNASCIMENTO.ValidatingType = typeof(DateTime);
         }
+
         private void ClearFields()
         {
             txtNOMECOMPLETO.Clear();
@@ -34,6 +30,7 @@ namespace SistemaOptometrico
             cbESCOLARIDADE.SelectedIndex = -1;
             cbSEXO.SelectedIndex = -1;
         }
+
         public int CalcularIdade(DateTime dataNascimento)
         {
             DateTime hoje = DateTime.Today;
@@ -65,46 +62,19 @@ namespace SistemaOptometrico
             string escolaridade = cbESCOLARIDADE.SelectedItem.ToString();
             string sexo = cbSEXO.SelectedItem.ToString();
 
-            // Comando SQL para inserir os dados na tabela tb_precadastro
-            string query = "INSERT INTO tb_precadastro (nome_completo, cpf, nascimento, idade, whatsapp, rua, bairro, cidade, profissao, escolaridade, sexo) " +
-                           "VALUES (@nome_completo, @cpf, @nascimento, @idade, @whatsapp,@rua, @bairro, @cidade, @profissao, @escolaridade, @sexo)";
-
-            // Conexão e comando com o banco de dados
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            // Criação da conexão e chamada do método de inserção
+            Conexao conexao = new Conexao();
+            try
             {
-                try
-                {
-                    connection.Open();
-                    using (MySqlCommand command = new MySqlCommand(query, connection))
-                    {
-                        // Adicionando parâmetros para prevenir SQL Injection
-                        command.Parameters.AddWithValue("@nome_completo", nomeCompleto);
-                        command.Parameters.AddWithValue("@cpf", cpf);
-                        command.Parameters.AddWithValue("@nascimento", nascimento.ToString("yyyy-MM-dd"));
-                        command.Parameters.AddWithValue("@idade", idade);
-                        command.Parameters.AddWithValue("@whatsapp", whatsapp);
-                        command.Parameters.AddWithValue("@rua", rua);
-                        command.Parameters.AddWithValue("@bairro", bairro);
-                        command.Parameters.AddWithValue("@cidade", cidade);
-                        command.Parameters.AddWithValue("@profissao", profissao);
-                        command.Parameters.AddWithValue("@escolaridade", escolaridade);
-                        command.Parameters.AddWithValue("@sexo", sexo);
+                conexao.InserirPreCadastro(nomeCompleto, cpf, nascimento, idade, whatsapp, rua, bairro, cidade, profissao, escolaridade, sexo);
+                MessageBox.Show("Dados salvos com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                        // Executa o comando de inserção
-                        command.ExecuteNonQuery();
-
-                        // Exibe uma mensagem de sucesso
-                        MessageBox.Show("Dados salvos com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                        // Limpa os campos do formulário
-                        ClearFields();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // Em caso de erro, exibe a mensagem de erro
-                    MessageBox.Show("Erro ao salvar os dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                // Limpa os campos do formulário
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar os dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
